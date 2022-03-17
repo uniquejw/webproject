e060f8e55630e3b59c014f2d2594d09997c14b92
uniquejw <jaewongogo@naver.com>
2022-03-18 03:25:45 +0900

    full stack test

---
+++ webproject002/app/src/main/java/webproject/web/dao/TodoDao.java
@@ -0,0 +1,37 @@
+package webproject.web.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import webproject.web.domain.Todo;
+
+@Mapper
+public interface TodoDao {
+
+  int countAll();
+
+  List<Todo> findAll();
+
+  int insert(Todo todo);
+
+  Todo findByNo(int no);
+
+  int update(Todo todo);
+
+  int updateDone(@Param("no") int no, @Param("done") boolean done);
+
+  int delete(int no);
+}
+
+
+
+
+
+
+
+
+
+
+
